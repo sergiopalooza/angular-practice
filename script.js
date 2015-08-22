df70dc18d516271bb0d1d608412d8ae49a51f149
Sergio Flores <sergioflores836@gmail.com>
2015-08-22 18:57:05 -0500

    added routing and controllers for the rest of the page

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 //creating module
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider){
+	$routeProvider 
+
+		//route for homepage
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'mainController'
+		})
+
+		.when('/about', {
+			templateUrl: 'pages/about.html',
+			controller: 'aboutController'
+		})
+
+		.when('/contact', {
+			templateUrl: 'pages/contact.html',
+			controller: 'contactController'
+		});
+});
 
 myApp.controller('mainController', function($scope){
 	$scope.message = "Hi I'm Sergio";
+});
+
+myApp.controller('aboutController', function($scope){
+	$scope.message = "About me!!!!";
+});
+
+myApp.controller('contactController', function($scope){
+	$scope.message = "Contact me?!?!?!?!";
 });
