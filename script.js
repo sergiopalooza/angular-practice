3e1c5ea0531ae5863002310b019daa1a4926cdb4
Sergio Flores <sergioflores836@gmail.com>
2015-08-22 19:56:35 -0500

    added styling and animations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //creating module
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate']);
 
 myApp.config(function($routeProvider){
 	$routeProvider 
@@ -9,12 +9,12 @@ myApp.config(function($routeProvider){
 			templateUrl: 'pages/home.html',
 			controller: 'mainController'
 		})
-
+		//route for about
 		.when('/about', {
 			templateUrl: 'pages/about.html',
 			controller: 'aboutController'
 		})
-
+		//route for contact
 		.when('/contact', {
 			templateUrl: 'pages/contact.html',
 			controller: 'contactController'
@@ -23,12 +23,15 @@ myApp.config(function($routeProvider){
 
 myApp.controller('mainController', function($scope){
 	$scope.message = "Hi I'm Sergio";
+	$scope.pageClass = 'homeStyle';
 });
 
 myApp.controller('aboutController', function($scope){
 	$scope.message = "About me!!!!";
+	$scope.pageClass = 'aboutStyle';
 });
 
 myApp.controller('contactController', function($scope){
 	$scope.message = "Contact me?!?!?!?!";
+	$scope.pageClass = 'contactStyle';
 });
